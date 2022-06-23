9f1c28a9887b98babb0a7c9f3902773167b51d68
pyrhaa <sboubtane@protonmail.com>
2022-06-23 12:31:04 +0200

    7.7 ok

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -17,12 +17,24 @@ const useField = (type) => {
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
-
+  const url = 'https://restcountries.com/v3.1/name/';
   useEffect(() => {
-    const url = 'https://restcountries.com/v3.1/name/';
-    axios
-      .get(`${url}/${name}?fullText=true`)
-      .then((res) => setCountry(res.data[0]));
+    const fetchData = async () => {
+      if (name) {
+        try {
+          const result = await axios.get(`${url}/${name}?fullText=true`);
+          console.log(result);
+          setCountry({ found: true, data: result.data[0] });
+        } catch (e) {
+          console.log('An error is there:', e);
+          console.log(
+            'You maybe wrongly or mispell or not fully correctly write the name of the country'
+          );
+          setCountry({ found: false, data: '' });
+        }
+      }
+    };
+    fetchData();
   }, [name]);
 
   return country;
@@ -30,18 +42,21 @@ const useCountry = (name) => {
 
 const Country = ({ country }) => {
   if (!country) {
+    return <div>Write a full country name in the research bar</div>;
+  }
+  if (!country.found) {
     return <div>not found...</div>;
   }
 
   return (
     <div>
-      <h3>{country.name.common}</h3>
-      <div>population {country.population}</div>
-      <div>capital {country.capital}</div>
+      <h3>{country.data.name.common}</h3>
+      <div>population {country.data.population}</div>
+      <div>capital {country.data.capital}</div>
       <img
-        src={country.flags.png}
+        src={country.data.flags.png}
         height="100"
-        alt={`flag of ${country.name.common}`}
+        alt={`flag of ${country.data.name.common}`}
       />
     </div>
   );
